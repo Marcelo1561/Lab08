280a485870b46e284589a954a8e62eb8cc296f12
Marcelo1561 <130408689+Marcelo1561@users.noreply.github.com>
2023-05-13 12:56:08 -0500

    Update Buscador.php

--- lab08/Producto/Buscador.php
+++ lab08/Producto/Buscador.php
@@ -1,29 +1,42 @@
 <?php
+// Conexión a la base de datos
+$conn = mysqli_connect("localhost", "root", "", "Eval02");
 
-    include ('ConexionBD.php');
+// Verificar la conexión
+if (!$conn) {
+    die("Error al conectar a la base de datos: " . mysqli_connect_error());
+}
+
+// Procesar el formulario de búsqueda
+if (isset($_GET['busqueda'])) {
+    $busqueda = $_GET['busqueda'];
+
+    // Consulta SQL para buscar coincidencias en la base de datos
+    $query = "SELECT * FROM Producto WHERE nombre LIKE '%$busqueda%' OR Descripcion LIKE '%$busqueda%'";
+    $result = mysqli_query($conn, $query);
+
+    // Mostrar los resultados de la búsqueda
+    if (mysqli_num_rows($result) > 0) {
+        echo "<h2>Resultados de la búsqueda:</h2>";
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<p>Nombre: " . $row['Nombre'] . "</p>";
+            echo "<p>Descripción: " . $row['Descripcion'] . "</p>";
+            echo "<p>Stock: " . $row['Stock'] . "</p>";
+            echo "<p>Precio de venta: " . $row['PrecioVenta'] . "</p>";
+            echo "<hr>";
+        }
+    } else {
+        echo "<p>No se encontraron resultados.</p>";
+    }
+}
+
+// Cerrar la conexión a la base de datos
+mysqli_close($conn);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-
-    <br><br><br>
-
-    <?php
-        if(isset($_GET['enviar'])){
-            $busqueda = $_GET['busqueda'];
-            $consulta =$con->query("SELECT * FROM Producto WHERE Nombre LIKE '%$busqueda%'");
-            while ($row = $consulta ->fetch_array()){
-                echo $row['Nombre'].'<br>';
-            }
-        }
-    ?>
-</body>
-</html>
+<!-- Formulario de búsqueda -->
+<form action="" method="GET">
+    <input type="text" name="busqueda" placeholder="Buscar productos">
+    <input type="submit" value="Buscar">
+</form>
